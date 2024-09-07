2086f051551e2253288001ad2c9b90c1d26ae234
igerne94 <igerne@gmail.com>
2024-09-07 23:35:54 +0200

    chore: Updated styles, page title; added Google Fonts link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,15 @@
-/* import { FC } from 'react'; */
-import './Button.module.css';
+import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
-
-/* export const ButtonAlt: FC<ButtonProps> = ({ className, children, ...props}) => {
-    return (
-        <button
-            className={cn('button accent', className)}
-            {...props}
-        >{children}</button>
-    );
-} */
 
 function Button({ children, className, ...props }: ButtonProps) {
     return (
         <button
-            className={cn('button accent', className)}
+            className={cn(
+                styles['button'],
+                styles['accent'],
+                className
+            )}
             {...props}
         >{children}</button>
     );
