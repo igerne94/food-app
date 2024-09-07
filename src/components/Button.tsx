5b246dd9b0e92d5b7cf5a58210a2ceb149bd22ab
igerne94 <igerne@gmail.com>
2024-09-07 22:15:52 +0200

    classnames package; upd Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+/* import { FC } from 'react'; */
+import './Button.module.css';
+import { ButtonProps } from './Button.props';
+import cn from 'classnames';
+
+/* export const ButtonAlt: FC<ButtonProps> = ({ className, children, ...props}) => {
+    return (
+        <button
+            className={cn('button accent', className)}
+            {...props}
+        >{children}</button>
+    );
+} */
+
+function Button({ children, className, ...props }: ButtonProps) {
+    return (
+        <button
+            className={cn('button accent', className)}
+            {...props}
+        >{children}</button>
+    );
+}
+
+export default Button;
