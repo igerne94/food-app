c399d5f0d2bd3cf725d4136d976e0f5e8ebfae50
igerne94 <igerne@gmail.com>
2024-09-08 17:56:19 +0200

    demo w Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import Button from './components/Button/Button'
-import Input from './components/Input/input';
+import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './components/Pages/Menu/Menu';
+import { Cart } from './components/Pages/Cart/Cart';
+import { ErrorM } from './components/Pages/Error/ErrorM';
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -15,7 +19,17 @@ function App() {
       <Button onClick={addCounter}>Click me</Button>
       <Button appearance="primary">Click me</Button>
       <p>{counter}</p>
-      <Input placeholder='email'/>
+      <Input placeholder='email' />
+      <div>
+        <a href='/'>Menu</a>
+        <a href='/cart'>Cart</a>
+      </div>
+      {/* Everything outside routing: ^ - is common */}
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="Cart" element={<Cart />} />
+        <Route path="*" element={<ErrorM />} />
+      </Routes>
     </>
   )
 }
