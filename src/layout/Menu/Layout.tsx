943e7b22a9967c2f6f0521d7a0aec63e27c27b40
igerne94 <igerne@gmail.com>
2024-09-09 18:15:34 +0200

    use NavLink with isActive property (better for navigation)

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,16 +1,9 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import styles from './Layout.module.css';
 import Button from "../../components/Button/Button";
-import { useEffect } from "react";
 import cn from 'classnames';
 
 export function Layout() {
-    const location = useLocation();
-
-    useEffect(() => { 
-        console.log(location.pathname);
-    }, [location]);
-
     return <div className={styles['layout']}>
         <div className={styles['sidebar']}>
             <div className={styles['user']}>
@@ -19,19 +12,30 @@ export function Layout() {
                 <div className={styles['email']}>Hanna@gmail.com</div>
             </div>
             <div className={styles['menu']}>
-                <Link
+                <NavLink
                     to='/'
-                    className={cn(styles['link'], {
-                    [styles['active']]: location.pathname === '/'
-                    })}
+                    className={
+                        ({ isActive }) => cn(
+                            styles['link'],
+                            { [styles['active']]: isActive }
+                        )
+                    }
                 >
                     <img src='/menu-icon.svg' alt='menu' />
                     Menu
-                </Link>
-                <Link className={styles['link']} to='/cart'>
+                </NavLink>
+                <NavLink
+                    to='/cart'
+                     className={
+                        ({ isActive }) => cn(
+                            styles['link'],
+                            { [styles['active']]: isActive }
+                        )
+                    }
+                >
                     <img src='/cart-icon.svg' alt='cart' />
                     Cart
-                </Link>
+                </NavLink>
             </div>
             <Button className={styles['exit']}>
                 <img src='/exit-icon.svg' alt='exit' />
