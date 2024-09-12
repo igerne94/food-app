69b85c017117c061ce1c20203f6e385c31733cfa
Hanna Khonskaya <48218270+igerne94@users.noreply.github.com>
2024-09-12 12:35:28 +0200

    ItemCard created and added to Menu; styled; typezied (#8)
    * Created a new ItemCard comp;
    * ItemCard: added styles, props typing;
    * Menu comp: added ItemCard
    
    Co-authored-by: igerne94 <igerne@gmail.com>

---
+++ src/components/ItemCard/ItemCard.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import styles from './ItemCard.module.css';
+import { ItemCardProps } from './ItemCard.props';
+
+function ItemCard(props: ItemCardProps) {
+    return (
+        <Link to='/' className={styles['link']}>
+            <div
+                className={styles['card']}
+                style={{ backgroundImage: `url('${props.image}}` }}
+            >
+                <div className={styles['head']} style={{backgroundImage: `url('${props.image}')`}}>
+                    <div className={styles['price']}>
+                        {props.price}&nbsp;
+                        <span className={styles['currency']}>€</span>
+                    </div>
+                    <button className={styles['add-to-card']}>
+                        <img src="/cart-button-icon.svg" alt="Add to cart" />
+                    </button>
+                    <div className={styles['rating']}>
+                        {props.rating}&nbsp;
+                        <img src="/star-icon.svg" alt="star-icon" />
+                    </div>
+                </div>
+
+                <div className={styles['footer']}>
+                    <div className={styles['title']}>{props.title}</div>
+                    <div className={styles['description']}>{props.description}</div>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default ItemCard;
--- src/components/Pages/Menu/Menu.tsx
+++ src/components/Pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Header from "../../Header/Header";
+import ItemCard from "../../ItemCard/ItemCard";
 import Search from "../../Search/Search";
 import styles from './Menu.module.css';
 
@@ -8,6 +9,16 @@ export function Menu() {
             <div className={styles['head']}>
                 <Header>Menu</Header>
                 <Search placeholder="Search by dish or ingredient" />
+            </div>
+            <div>
+                <ItemCard
+                    id={1}
+                    title="Pizza"
+                    description="Italian pizza with olives"
+                    price={10}
+                    rating={4}
+                    image="/product-demo.png"
+                />
             </div>
         </>)
 }
