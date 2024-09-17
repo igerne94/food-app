5434b01ed13081f9bf09f5c6e9ec23c7b4bd0204
igerne94 <igerne@gmail.com>
2024-09-17 20:59:59 +0200

    Login layout

--- src/components/Pages/Login/Login.tsx
+++ src/components/Pages/Login/Login.tsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+import Button from "../../Button/Button";
+import Header from "../../Header/Header";
+import Input from "../../Input/Input";
+import styles from './Login.module.css';
+
 export function Login() {
-    return <>Login</>
+    return (
+        <div className={styles['login']}>
+            <Header>Login</Header>
+            <form className={styles['form']}>
+                <div className={styles['field']}>
+                    <label htmlFor="">Your email</label>
+                    <Input placeholder="Email" id="email"/>
+                </div>
+                <div className={styles['field']}>
+                    <label htmlFor="">Your password</label>
+                    <Input placeholder="Password" id="password" type="password"/>
+                </div>
+                <Button appearance="primary">Login</Button>
+            </form>
+            <div className={styles['links']}>
+                <div>Don't have an account?</div>
+                <Link to={"/authorization/register"}>Register</Link>
+            </div>
+        </div>
+    );
 }
