cc0dfe3b47446b2c3deb90855ecf6b9520c25603
igerne94 <igerne@gmail.com>
2024-09-19 14:22:53 +0200

    Added user slice (w 2 reducers) to Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./user.slice";
 
 export const store = configureStore({
-    reducer: { }
+    reducer: {
+        user: userSlice
+    }
 });
 
 console.log(store.getState());
